c65fae97c978e3012938b001b6acd82b84979a91
Prudhvi Karanam <prudhvikaranam537@gmail.com>
2019-01-17 18:20:09 +0530

    adding image from URL in input field

--- Constructors,Prototypes,ES6 Classes and subclasses/bookProject.js
+++ Constructors,Prototypes,ES6 Classes and subclasses/bookProject.js
@@ -4,21 +4,26 @@ function Book(id,name,company,telephone,companyid,image){
   this.company = company;
   this.telephone = telephone;
   this.companyid = companyid;
+  this.image = image;
 }
 
 function UI(){}
 UI.prototype.addBookToList = function(book1){
   const list = document.getElementById('emp-list');
   const row = document.createElement('tr');
+  const image = document.createElement('img');
+  image.setAttribute('src',book1.image.value);
+  image.width = '25';
   row.innerHTML = `
     <td>${book1.id}</td>
     <td>${book1.name}</td>
     <td>${book1.company}</td>
     <td>${book1.telephone}</td>
     <td>${book1.companyid}</td>
-
     `
   list.appendChild(row);
+  list.appendChild(image);
+
 }
 var id = 0;
 
@@ -36,9 +41,9 @@ document.getElementById('form').addEventListener('submit',
           }else{
             companyid = 'Metioned Company has not yet assigned a Company ID';
           }
+    var image = document.getElementById('image');
     
-    
-    var book = new Book(id,name,company,telephone,companyid);
+    var book = new Book(id,name,company,telephone,companyid,image);
     var ui = new UI();
     ui.addBookToList(book);
     e.preventDefault();
@@ -89,6 +94,7 @@ document.getElementById('form').addEventListener('submit',
 
 
 // // Without a prototype method
+
 // function Book(id,name,company,telephone,companyid,image){
 //   this.id = id;
 //   this.name = name;
@@ -133,6 +139,46 @@ document.getElementById('form').addEventListener('submit',
 //     var ui = new UI(book);
  
 //     console.log(ui.book);
+//     e.preventDefault();
+//   }
+// );
+
+
+// // Without creating a UI constructor
+
+// function Book(id,name,company,telephone,companyid,image){
+//   this.id = id;
+//   this.name = name;
+//   this.company = company;
+//   this.telephone = telephone;
+//   this.companyid = companyid;
+// }
+// var id = 0;
+// document.getElementById('form').addEventListener('submit',
+//   function(e){
+//     var name = document.getElementById('name').value,
+//      company = document.getElementById('company').value,
+//      telephone = document.getElementById('telephone').value;
+//       id = id+1;
+//     var companyid;
+//           if(company === 'Altisource'){
+//               companyid = `##1`;
+//           }else if(company === 'tvsr'){
+//             companyid = '##5';
+//           }else{
+//             companyid = 'Metioned Company has not yet assigned a Company ID';
+//           }
+//     var book = new Book(id,name,company,telephone,companyid);
+//     const list = document.getElementById('emp-list');
+//     const row = document.createElement('tr');
+//     row.innerHTML = `
+//       <td>${book.id}</td>
+//       <td>${book.name}</td>
+//       <td>${book.company}</td>
+//       <td>${book.telephone}</td>
+//       <td>${book.companyid}</td>
+//       `
+//   list.appendChild(row);
 //     e.preventDefault();
 //   }
 // );
