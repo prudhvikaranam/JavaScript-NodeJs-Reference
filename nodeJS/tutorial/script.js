707cca8b7019bf1d0723759948ed60fc2f25eb7a
Prudhvi Karanam <prudhvikaranam537@gmail.com>
2023-09-05 00:56:15 +0530

    adding event loop concepts for I/O and setimmediate queues

--- nodeJS/tutorial/script.js
+++ nodeJS/tutorial/script.js
@@ -1,3 +1,10 @@
+// *****************************************************
+// If we want to go back one directory using the __dirname, we need to make use of path like below
+// For example, we are in some other upfront (like eventloop folder) directory and want to use the input file which is in the backward (main... txt) directory then we have to use below way in the file which is in eventloop folder.
+// const path = require('path');
+// console.log(`${path.join(__dirname,'../txt/input.txt')}`);
+
+
 // ///////////////////////////////////////////////////////////////////////////////////////////////////
 // // ----------------------------Reading and writing files
 // // We have two version of reading and writing files
