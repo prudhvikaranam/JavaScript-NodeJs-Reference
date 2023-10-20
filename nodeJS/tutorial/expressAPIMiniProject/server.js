40e2076176786816638934399d8658fc8e4f4e3a
Prudhvi Karanam <prudhvikaranam537@gmail.com>
2023-10-20 17:11:08 +0530

    adding about MVC architecture

--- nodeJS/tutorial/expressAPIMiniProject/server.js
+++ nodeJS/tutorial/expressAPIMiniProject/server.js
@@ -6,27 +6,6 @@ const DB = process.env.MONGO_DATABASE_URL.replace('<PASSWORD>',process.env.MONGO
 
 console.log(DB)
 
-mongoose.connect(DB).then((data) => {
-    console.log('DB connected');
-}).catch((err) => {
-    console.log(`Error`,err);
-})
-
-const schema = new mongoose.Schema({
-    'name' : String,
-    'age' : Number
-})
-
-const tester2 = mongoose.model('tester1',schema);
-
-const testUser3 = new tester2({
-    'name' : 'Ramu',
-    'age' : 10000
-})
-
-testUser3.save().then((data)  => {console.log(data)}).catch((err) => {console.log(err)});
-
-
 app.listen(3030, '127.0.0.2', () => {
     console.log(`Express API mini Project started running!!!!`);
 })
