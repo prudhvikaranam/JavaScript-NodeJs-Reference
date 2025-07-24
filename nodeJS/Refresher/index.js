05062bb2f039135c1ab40b2d83cb000d66302d37
Prudhvi Teja Karanam <prudhvikaranam537@gmail.com>
2025-07-24 20:55:07 +0530

    adding refresher to node js

---
+++ nodeJS/Refresher/index.js
@@ -0,0 +1,40 @@
+// mongodb+srv://prudhviteja7443:vYp2bdKpoYT7od1v@namasteuserscluster.8t1dcem.mongodb.net/
+// mongodb+srv://prudhviteja7443:<db_password>@namasteuserscluster.8t1dcem.mongodb.net/
+
+const http = require('http');
+const { MongoClient } = require("mongodb");
+
+const url = "mongodb+srv://prudhviteja7443:vYp2bdKpoYT7od1v@namasteuserscluster.8t1dcem.mongodb.net/"
+
+const client = new MongoClient(url);
+async function run() {
+    try {
+        // Connect to the Atlas cluster
+         await client.connect();
+         // Get the database and collection on which to run the operation
+         const db = client.db("NamastheUsers");
+         const col = db.collection("users");
+         // Create new documents                                                                                                                                         
+         // Find the document
+         const filter = { };
+         const document = await col.findOne(filter);
+         // Print results
+         console.log("Document found:\n" + JSON.stringify(document));
+        } catch (err) {
+         console.log(err.stack);
+     }
+ 
+     finally {
+        await client.close();
+    }
+ 
+}
+run().catch(console.dir);
+
+
+http.createServer((req, res) => {
+    console.log('Prudhvi server running');
+    res.end('App running');
+}).listen(8080, '127.0.0.2', () => {
+    console.log('server startedddddd');
+})
